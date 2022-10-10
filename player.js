388f06372a5c87a7647ab4a40fb17fd12c2d8865
Malik Washington <malikwashington@gmail.com>
2022-10-10 14:08:37 -0400

    added states

--- player.js
+++ player.js
@@ -1,17 +1,31 @@
+import {
+  StandingLeft,
+  StandingRight,
+  SittingLeft,
+  SittingRight,
+  RunningLeft,
+  RunningRight
+} from "./state.js";
 export default class Player {
   constructor(gameWidth, gameHeight) {
-    this.gameWidth = gameWidth
-    this.gameHeight = gameHeight
-    this.states = []
-    this.currentState = this.states[0]
-    this.image = document.getElementById('player')
-    this.width = 200
-    this.height = 181.83
-    this.x = (this.gameWidth-this.width)/2
-    this.frameX = 0
-    this.y = this.gameHeight - this.height
-    this.frameY = 0
-
+    this.gameWidth = gameWidth;
+    this.gameHeight = gameHeight;
+    this.states = [
+      new StandingLeft(this),
+      new StandingRight(this),
+      new SittingLeft(this),
+      new SittingRight(this),
+      new RunningLeft(this),
+      new RunningRight(this)
+    ];
+    this.currentState = this.states[1];
+    this.image = document.getElementById("player");
+    this.width = 200;
+    this.height = 181.83;
+    this.x = (this.gameWidth - this.width) / 2;
+    this.frameX = 0;
+    this.y = this.gameHeight - this.height;
+    this.frameY = 0;
   }
   draw(context) {
     context.drawImage(
@@ -25,5 +39,12 @@ export default class Player {
       this.width,
       this.height
     );
+  }
+  update(input) {
+    this.currentState.handleInput(input);
+  }
+  setState(state) {
+    this.currentState = this.states[state];
+    this.currentState.enter();
   }
 }
